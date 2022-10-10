9ed0951cc5783a79f7d1ec4faa23cd8ef1c95bce
Naga Sai Chaitanya <55658505+naga-sai-chaitanya@users.noreply.github.com>
2022-10-10 13:45:12 +0530

    Update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,37 +4,71 @@ document.getElementById('fund-transfer-div').style.display = 'none';
 
 const myTransBtn = document.getElementById('trans-btn');
 
+const customTransactionBtn = document.getElementById('custom-transactions');
+
 myTransBtn.onclick = () =>{
+  showTransactions()
+}
+
+customTransactionBtn.onclick = () => {
+  const from = document.getElementById('from-dt').value;
+  const to = document.getElementById('to-dt').value;
+  showTransactions(from,to)
   
+}
+
+
+
+    
+const showTransactions = (from,to) => {
   document.getElementById('trans-div').style.display = 'block';
   document.getElementById('fund-transfer-div').style.display = 'none';
-  fetch('http://localhost:4060/transaction').then(res => res.json())
-                                          .then(data => {
-                                            console.log('data',data);
-                                            var myTble = document.getElementById('trans-table')
-                                            var tBody = document.createElement('tbody');
-                                            for(var i=0;i<data.length;i++){
-                                              const {date,accountNo,name,Received,Sent} = data[i];
-                                              var values = [i+1,date,accountNo,name,Received,Sent];
-                                              
-                                              var trEle = document.createElement('tr');
-                        
-                                              for(var j=0;j<6;j++){
-                                                var tdEle = document.createElement('td')
-                                                tdEle.textContent = values[j]
-                                                trEle.appendChild(tdEle)
-                                              }
-                                              tBody.appendChild(trEle);
-                                              
-                                            }
-                                            myTble.appendChild(tBody);
-                                          })
+  if(!from && !to){
+  
+    fetch('/transaction').then(res => res.json())
+                                              .then(data => populateTable(data))
+  }
+  else{
+    fetch(`/transactions/user?from=${from}&to=${to}`).then(res => res.json())
+                                                                    .then(data => populateTable(data));
+  }
+}
 
+const populateTable = (data) => {
+  if(data.length == 0){
+    return document.getElementById("trans-table").textContent = "No Data Found"
+  }
+  document.getElementById("trans-table").textContent = "";
+  const theadEle = document.createElement('thead');
+  
+  var myTble = document.getElementById('trans-table')
+  var col_names = ['S.No','Date','Account No', 'Account Holder','Received','Sent'];
+  for(var j=0;j<col_names.length;j++){
+    const thEle = document.createElement('th')
+    thEle.textContent = col_names[j];
+    theadEle.appendChild(thEle)
+  }
+  myTble.appendChild(theadEle);
+
+  var tBody = document.createElement('tbody');
+  for(var i=0;i<data.length;i++){
+    const {date,accountNo,name,Received,Sent} = data[i];
+    var dt = new Date(date).toDateString();
+    
+    var values = [i+1,dt,accountNo,name,Received,Sent];
+    var trEle = document.createElement('tr');
+    for(var j=0;j<6;j++){
+      var tdEle = document.createElement('td')
+      tdEle.textContent = values[j]
+      trEle.appendChild(tdEle)
+    }
+    tBody.appendChild(trEle);
+  }
+  myTble.appendChild(tBody);
 }
 
 const fundTransferBtn = document.getElementById('fund-transfer-btn');
 fundTransferBtn.onclick = () => {
-  console.log('fund transfer button clicked...')
   document.getElementById('trans-div').style.display = 'none';
   document.getElementById('fund-transfer-div').style.display = 'block';
 
@@ -49,7 +83,7 @@ fundTransferBtn.onclick = () => {
           contentType: "application/json; charset=UTF-8",
           timeout: 1000,
           redirect:true,
-          url: 'http://localhost:4060/transfer', 
+          url: '/transfer', 
           success: function (data) {
             
             if(data.status == 'failed'){
@@ -61,53 +95,19 @@ fundTransferBtn.onclick = () => {
               document.getElementById('trans-status').style.color = 'green';
               document.getElementById('trans-status').textContent = data.msg;
             }
-              
-             
-  
           },
            error: function (error) {
-              console.log('error',error)
-              
+              console.log('error',error)    
          }
       })
-      
   }
-
 }
 
 
-
-
-
-
-/*
-const myTable = document.getElementById('trans-table')
-fetch('http://localhost:4060/transaction').then(res => res.json())
-                                          .then(data => {
-                                            console.log('data',data);
-                                            var myTble = document.getElementById('trans-table')
-                                            var tBody = document.createElement('tbody');
-                                            for(var i=0;i<data.length;i++){
-                                              const {date,accountNo,name,Received,Sent} = data[i];
-                                              var values = [i+1,date,accountNo,name,Received,Sent];
-                                              
-                                              var trEle = document.createElement('tr');
-                        
-                                              for(var j=0;j<6;j++){
-                                                var tdEle = document.createElement('td')
-                                                tdEle.textContent = values[j]
-                                                trEle.appendChild(tdEle)
-                                              }
-                                              tBody.appendChild(trEle);
-                                              
-                                            }
-                                            myTble.appendChild(tBody);
-                                          })
-*/
 const dis_bal_btn = document.getElementById('bal-button')
 
 dis_bal_btn.onclick = () => {
-    fetch('http://localhost:4060/balance').then(res=>res.json())
+    fetch('/balance').then(res=>res.json())
                                           .then(data=>{
                                             document.getElementById('display-bal').textContent = data.balance;
                                           })
